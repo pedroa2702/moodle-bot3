52cd9daf45169b5b145fdda203168f504501a563
Tnoob-dev <93684341+Tnoob-dev@users.noreply.github.com>
2022-05-17 19:44:27 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = os.environ.get("API_ID", "")
 API_HASH = os.environ.get("API_HASH", "")
 
 
 # DATOS DEL MOODLE
 USUARIO = os.environ.get("USUARIO_MOODLE", "")
 PASSWORD = os.environ.get("PASSW_MOODLE", "")
-USUARIO_ID = int(os.environ.get("ID_MOODLE", ""))
-ZIP_MB = int(os.environ.get("TAMANO_ZIP", ""))
+USUARIO_ID = os.environ.get("ID_MOODLE", "")
+ZIP_MB = os.environ.get("TAMANO_ZIP", "")
 MOODLE_URL = os.environ.get("MOODLE_URL", "")
 
 
 
-OWNER = int(os.environ.get("ID_TELEGRAM", ""))
+OWNER = os.environ.get("ID_TELEGRAM", "")
